51a4443ef8ac321c5f64dbf673f940d3d4df84f3
Tejas <83050961+Tejas-Solanki3@users.noreply.github.com>
2025-05-26 17:04:46 +0530

    arithmetic

---
+++ Operators/Arithmetic.java
@@ -0,0 +1,17 @@
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / yl
+        //z = x % y;
+        x = x + y;
+        System.out.println(x);
+
+    }
+}
